a50a1e06b6dc90af3fc80e81d0fc9ae83a1e82b3
Miles Zimmerman <miles@zserver.org>
2014-03-31 15:00:47 -0700

    configure logging

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+var sugar   = require('sugar');
+var winston = require('winston');
+
+/**
+ * Configure logging
+ */
+winston.remove(winston.transports.Console);
+winston.add(winston.transports.Console, {
+  colorize: true,
+  timestamp: function() {
+    return Date.create().format('{yyyy}-{MM}-{dd} {HH}:{mm}:{ss}.{fff}');
+  }
+});
+
+// Export
 module.exports = require('./lib/wordpress_service.js');
